985166a01be6132410b243c51a62b0e3d06fe3f6
Sona Mathew <sonamathewc@gmail.com>
2022-12-01 21:41:32 +0530

    complete register login jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import {
     db
 } from "../db.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export const register = (req, res) => {
     const q = "SELECT * FROM users WHERE email=? OR username = ?"
@@ -29,6 +30,29 @@ export const register = (req, res) => {
 
 export const login = (req, res) => {
 
+    const q = "SELECT * FROM users WHERE username = ?"
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) return res.json(err);
+        if (data.length === 0) return res.status(404).json("User not found!");
+
+        //Check Password
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if (!isPasswordCorrect)
+            return res.status(400).json("Wrong username or password!")
+        const token = jwt.sign({
+            id: data[0].id
+        }, "jwtkey");
+        const {
+            password,
+            ...other
+        } = data[0];
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(other)
+    })
 }
 
 export const logout = (req, res) => {
--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ import express from "express"
 import authRoutes from "./routes/auth.js"
 import flightRoutes from "./routes/auth.js"
 import userRoutes from "./routes/auth.js"
+import cookieParser from "cookie-parser"
 
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/flights", flightRoutes)
 app.use("/api/user", userRoutes)
